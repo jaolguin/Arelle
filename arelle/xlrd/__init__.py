3be5429b52477b0e8794e5a0e69c7446bf91bf12
Ryan Heimbuch <rheimbuch@gmail.com>
2012-02-21 23:23:19 -0700

    remove unicode copyright char

--- arelle/xlrd/__init__.py
+++ arelle/xlrd/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright © 2005-2009 Stephen John Machin, Lingfo Pty Ltd
+# Copyright (c) 2005-2009 Stephen John Machin, Lingfo Pty Ltd
 # This module is part of the xlrd3 package, which is released under a
 # BSD-style licence.
 #
--- arelle/xlrd/xldate.py
+++ arelle/xlrd/xldate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # No part of the content of this file was derived from the works of David Giffin.
 #
-# Copyright © 2005-2008 Stephen John Machin, Lingfo Pty Ltd
+# Copyright (c) 2005-2008 Stephen John Machin, Lingfo Pty Ltd
 # This module is part of the xlrd3 package, which is released under a
 # BSD-style licence.
 #

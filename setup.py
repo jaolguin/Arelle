42198b675d0d3ea50959d2a99a22eb39c57d0fa0
Ryan Heimbuch <rheimbuch@gmail.com>
2012-02-21 22:46:25 -0700

    try using lib3to2.main.StdoutRefactoringTool

--- setup.py
+++ setup.py
@@ -15,12 +15,16 @@ from distutils.command.build_py import build_py as _build_py
 if sys.version_info[0] < 3:
     setup_requires.append('3to2')
 
+
+
+
 # If under python 2.7, run refactorings from lib3to2
 class build_py27(_build_py):
     def __init__(self, *args, **kwargs):
         _build_py.__init__(self, *args, **kwargs)
-        from lib2to3 import refactor        
-        self.rtool = refactor.RefactoringTool(
+        from lib2to3 import refactor
+        import lib3to2.main
+        self.rtool = lib3to2.main.StdoutRefactoringTool(
             refactor.get_fixers_from_package('lib3to2.fixes')
             )
     
